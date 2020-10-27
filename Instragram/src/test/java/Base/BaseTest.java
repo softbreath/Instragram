f4052546c64e1e7833b100e4d72bf1c1713dc7cb
Dell <Dell@DESKTOP-5HMC83B>
2020-10-27 17:20:13 -0400

    Add screenshots to the BaseTest class

--- Instragram/src/test/java/Base/BaseTest.java
+++ Instragram/src/test/java/Base/BaseTest.java
@@ -1,7 +1,14 @@
 package Base;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,5 +25,25 @@ public class BaseTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
+	
+	
+	
+	public void takeScreenShot(){
+		 //Take screenshot and keep it on the memory location called File
+		 File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		 // To get a name for screenshots as timestamp
+		 String time = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss").format(new Date());
+		
+		 try {
+		 //copy the screenshot from File and paste in the given location");
+		
+		 FileUtils.copyFile(src, new File("c:\\image\\"+ time + "pic.png"));
+		
+		 } catch (IOException e) {
+		 e.printStackTrace();
+		 }
+
+		 }
 
 }
---
+++ Instragram/src/test/java/test/test01.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+import Base.BaseTest;
+
+public class test01 extends BaseTest{
+	
+	@Test
+	public void test(){
+		init();
+		takeScreenShot();
+		
+		
+	}
+	
+
+}
